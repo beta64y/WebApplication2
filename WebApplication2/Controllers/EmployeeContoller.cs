8b29ca059da9659d2abdb6f28da9f0a576526dc9
Yəhya Camalzadə <camalzadeyahya8@gmail.com>
2024-02-25 23:49:32 +0400

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeContoller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Contexts;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeContoller : Controller
+    {
+        private readonly AppDbContext _context;
+        public EmployeeContoller(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> employees = _context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
